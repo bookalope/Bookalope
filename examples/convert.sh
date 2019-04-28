34969b4dd7eb49947286556b7cf672240ea9928f
Jens Troeger <jens.troeger@light-speed.de>
2019-04-27 17:00:05 -0700

    Migrate goodness from epubdate.sh to the convert.sh example.

--- examples/convert.sh
+++ examples/convert.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]; then
-    echo -e "Missing argument(s)\nUsage: $0 token document"
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+    echo -e "Missing argument(s)\nUsage: $0 token document [type]"
     exit 1
 fi
 
 TOKEN=$1
 DOCFILE=$2
+DOCTYPE=${3:-"doc"}
 TMPDIR=$(mktemp -d)
 
 # Make sure that the document actually exists.
 if [ ! -f "$DOCFILE" ]; then
     echo "Document $DOCFILE does not exist, exiting"
+    exit 1
+fi
+
+# Make sure that the document type is one supported by Bookalope.
+if [[ ! "$DOCTYPE" =~ ^(doc|epub|gutenberg)$ ]]; then
+    echo "Document type '$DOCTYPE' is not one of 'doc', 'epub', 'gutenberg'"
     exit 1
 fi
 
@@ -29,7 +36,7 @@ fi
 
 # The Bookalope server.
 APIHOST="https://bookflow.bookalope.net"
-echo "Talking to server $APIHOST"
+echo "Talking to Bookalope server $APIHOST"
 
 # Separate path, filename, and extension of the document.
 DOCPATH=$(dirname "$DOCFILE")
@@ -46,25 +53,21 @@ if [ `builtin type -p http` ]; then
 
     # Create a new book.
     echo "Creating new Book..."
-    BOOKID=`http --json --print=b --auth $TOKEN: POST $APIHOST/api/books name="$DOCBASE" | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['book']['id']);"`
-    echo "Done, id=$BOOKID"
-
-    # Create a new bookflow "Bookflow 1" for the book.
-    echo "Creating new Bookflow..."
-    BOOKFLOWID=`http --json --print=b --auth $TOKEN: POST $APIHOST/api/books/$BOOKID/bookflows name="Bookflow 1" title="$DOCBASE" | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['bookflow']['id']);"`
-    echo "Done, id=$BOOKFLOWID"
+    read -r BOOKID BOOKFLOWID <<< `http --ignore-stdin --json --print=b --auth $TOKEN: POST $APIHOST/api/books name="$DOCBASE" | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['book']['id'], obj['book']['bookflows'][0]['id']);"`
+    echo "Done, Book id=$BOOKID, Bookflow id=$BOOKFLOWID"
 
     # Upload the manuscript which automatically converts it using defaults.
-    echo "Uploading and analyzing book document..."
+    echo "Uploading and analyzing document: $DOCNAME"
     base64 "$DOCFILE" > "$TMPDIR/$DOCNAME.base64"
-    http --json --auth $TOKEN: POST $APIHOST/api/bookflows/$BOOKFLOWID/files/document file=@"$TMPDIR/$DOCNAME.base64" filename="$DOCNAME" filetype=doc
+    http --ignore-stdin --json --print= --auth $TOKEN: POST $APIHOST/api/bookflows/$BOOKFLOWID/files/document file=@"$TMPDIR/$DOCNAME.base64" filename="$DOCNAME" filetype="$DOCTYPE"
 
     # Wait until the bookflow's step changes from 'processing' to 'convert'.
-    echo "Waiting for bookflow to finish..."
-    while [ 1 ]; do
+    echo "Waiting for Bookflow to finish..."
+    while true; do
         sleep 5
-        STEP=`http --json --print=b --auth $TOKEN: GET $APIHOST/api/bookflows/$BOOKFLOWID | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['bookflow']['step']);"`
+        STEP=`http --ignore-stdin --json --print=b --auth $TOKEN: GET $APIHOST/api/bookflows/$BOOKFLOWID | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['bookflow']['step']);"`
         if [ "$STEP" = "convert" ]; then
+            echo "Done"
             break
         fi
         if [ "$STEP" = "processing_failed" ]; then
@@ -72,29 +75,28 @@ if [ `builtin type -p http` ]; then
             exit 1
         fi
     done
-    echo "Done"
 
     # Convert and download the books in a parallel batch.
     # Regarding < /dev/tty see: https://github.com/jakubroztocil/httpie/issues/150#issuecomment-21419373
     echo "Converting and downloading books..."
     function convert_book {
-        DOWNLOAD_URL=`http --auth $TOKEN: POST $APIHOST/api/bookflows/$BOOKFLOWID/convert format=$1 version=test < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['download_url'])"`
-        while [ 1 ]; do
+        local DOWNLOAD_URL=`http --ignore-stdin --print=b --auth $TOKEN: POST $APIHOST/api/bookflows/$BOOKFLOWID/convert format=$1 version=test < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['download_url'])"`
+        while true; do
             sleep 5
-            STATUS=`http --auth $TOKEN: GET $DOWNLOAD_URL/status < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['status']);"`
+            STATUS=`http --ignore-stdin --print=b --auth $TOKEN: GET $DOWNLOAD_URL/status < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['status']);"`
             case "$STATUS" in
             "processing")
                 ;;
             "failed")
-                echo "Bookalope failed to convert the book, exiting"
-                exit 1
+                echo "Bookalope failed to convert the book to $1, skipping"
+                break
                 ;;
             "ok")
+                http --download --ignore-stdin --print= --auth $TOKEN: GET $DOWNLOAD_URL > /dev/tty
                 break
                 ;;
             esac
         done
-        http --download --auth $TOKEN: GET $DOWNLOAD_URL > /dev/tty
     }
     export -f convert_book
     export TOKEN
@@ -104,8 +106,8 @@ if [ `builtin type -p http` ]; then
     echo "Done"
 
     # Delete the book and all of its bookflows.
-    echo "Deleting book and bookflows..."
-    http --auth $TOKEN: DELETE $APIHOST/api/books/$BOOKID
+    echo "Deleting Book and Bookflows..."
+    http --ignore-stdin --print= --auth $TOKEN: DELETE $APIHOST/api/books/$BOOKID
     echo "Done"
 
 else
@@ -113,35 +115,30 @@ else
     if [ `builtin type -p curl` ]; then
 
         # Check that the token authenticates correctly.
-        if [ ! `curl --user $TOKEN: --request GET -s -D - -o /dev/null $APIHOST/api/profile | grep HTTP | cut -d ' ' -f 2` == "200" ]; then
+        if [ ! `curl --silent --show-error --user $TOKEN: --request GET -s -D - -o /dev/null $APIHOST/api/profile | grep HTTP | cut -d ' ' -f 2` == "200" ]; then
             echo "Wrong Bookalope API token, exiting"
             exit 1
         fi
 
         # Create a new book.
         echo "Creating new Book..."
-        BOOKID=`curl --user $TOKEN: --header "Content-Type: application/json" --data '{"name":"'$DOCBASE'"}' --request POST $APIHOST/api/books | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['book']['id']);"`
-        echo "Done, id=$BOOKID"
-
-        # Create a new bookflow "Bookflow 1" for the book.
-        echo "Creating new Bookflow..."
-        BOOKFLOWID=`curl --user $TOKEN: --header "Content-Type: application/json" --data '{"name":"Bookflow 1", "title":"'$DOCBASE'"}' --request POST $APIHOST/api/books/$BOOKID/bookflows | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['bookflow']['id']);"`
-        echo "Done, id=$BOOKFLOWID"
+        read -r BOOKID BOOKFLOWID <<< `curl --silent --show-error --user $TOKEN: --header "Content-Type: application/json" --data '{"name":"'$DOCBASE'"}' --request POST $APIHOST/api/books | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['book']['id'], obj['book']['bookflows'][0]['id']);"`
+        echo "Done, Book id=$BOOKID, Bookflow id=$BOOKFLOWID"
 
         # Upload the manuscript which automatically converts it using defaults.
         echo "Uploading and analyzing book document..."
-        echo '{"filetype":"doc", "filename":"'$DOCNAME'", "file":"' > "$TMPDIR/$DOCNAME.json"
+        echo '{"filetype":"'$DOCTYPE'", "filename":"'$DOCNAME'", "file":"' > "$TMPDIR/$DOCNAME.json"
         base64 "$DOCFILE" >> "$TMPDIR/$DOCNAME.json"
         echo '"}' >> "$TMPDIR/$DOCNAME.json"
-        curl --user $TOKEN: --header "Content-Type: application/json" --data @"$TMPDIR/$DOCNAME.json" --request POST $APIHOST/api/bookflows/$BOOKFLOWID/files/document
-        echo "Done"
+        curl --silent --show-error -output /dev/null --user $TOKEN: --header "Content-Type: application/json" --data @"$TMPDIR/$DOCNAME.json" --request POST $APIHOST/api/bookflows/$BOOKFLOWID/files/document
 
         # Wait until the bookflow's step changes from 'processing' to 'convert'.
-        echo "Waiting for bookflow to finish..."
-        while [ 1 ]; do
+        echo "Waiting for Bookflow to finish..."
+        while true; do
             sleep 5
-            STEP=`curl --user $TOKEN: --header "Content-Type: application/json" --request GET $APIHOST/api/bookflows/$BOOKFLOWID | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['bookflow']['step']);"`
+            STEP=`curl --silent --show-error --user $TOKEN: --header "Content-Type: application/json" --request GET $APIHOST/api/bookflows/$BOOKFLOWID | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['bookflow']['step']);"`
             if [ "$STEP" = "convert" ]; then
+                echo "Done"
                 break
             fi
             if [ "$STEP" = "processing_failed" ]; then
@@ -149,40 +146,41 @@ else
                 exit 1
             fi
         done
-        echo "Done"
 
         # Convert and download the books in a parallel batch.
         # Regarding < /dev/tty see: https://github.com/jakubroztocil/httpie/issues/150#issuecomment-21419373
         echo "Converting and downloading books..."
         function convert_book {
-            DOWNLOAD_URL=`curl --user $TOKEN: --header "Content-Type: application/json" --data '{"format":"'$1'", "version":"test"}' --request POST $APIHOST/api/bookflows/$BOOKFLOWID/convert < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['download_url'])"`
-            while [ 1 ]; do
+            local DOWNLOAD_URL=`curl --silent --show-error --user $TOKEN: --header "Content-Type: application/json" --data '{"format":"'$1'", "version":"test"}' --request POST $APIHOST/api/bookflows/$BOOKFLOWID/convert < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['download_url'])"`
+            while true; do
                 sleep 5
-                STATUS=`curl --user $TOKEN: --header "Content-Type: application/json" --request GET $DOWNLOAD_URL/status < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['status']);"`
+                STATUS=`curl --silent --show-error --user $TOKEN: --header "Content-Type: application/json" --request GET $DOWNLOAD_URL/status < /dev/tty | python3 -c "import json,sys;obj=json.load(sys.stdin);print(obj['status']);"`
                 case "$STATUS" in
                 "processing")
                     ;;
                 "failed")
-                    echo "Bookalope failed to convert the book, exiting"
-                    exit 1
+                    echo "Bookalope failed to convert the book to $1, skipping"
+                    break
                     ;;
                 "ok")
+                    curl --silent --show-error --user $TOKEN: --remote-name --remote-header-name --request GET $DOWNLOAD_URL > /dev/tty
                     break
                     ;;
                 esac
             done
-            curl --user $TOKEN: --remote-name --remote-header-name --request GET $DOWNLOAD_URL > /dev/tty
         }
         export -f convert_book
         export TOKEN
         export APIHOST
         export BOOKFLOWID
-        parallel --env TOKEN --env APIHOST --env BOOKFLOWID ::: "convert_book \"epub\"" "convert_book \"epub3\"" "convert_book \"mobi\"" "convert_book \"pdf\"" "convert_book \"icml\"" "convert_book \"docx\"" "convert_book \"docbook\"" "convert_book \"htmlbook\""
+        # Unlike with `http` we do not request EPUB2 from the Bookalope server because curl does not
+        # handle existing files well: "epub" and "epub3" produce the same local file name.
+        parallel --env TOKEN --env APIHOST --env BOOKFLOWID ::: "convert_book \"epub3\"" "convert_book \"mobi\"" "convert_book \"pdf\"" "convert_book \"icml\"" "convert_book \"docx\"" "convert_book \"docbook\"" "convert_book \"htmlbook\""
         echo "Done"
 
         # Delete the book and all of its bookflows.
-        echo "Deleting book and bookflows..."
-        curl --user $TOKEN: --request DELETE $APIHOST/api/books/$BOOKID
+        echo "Deleting Book and Bookflows..."
+        curl --silent --show-error --user $TOKEN: --request DELETE $APIHOST/api/books/$BOOKID
         echo "Done"
 
     else
